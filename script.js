e67706264258e41b7a01ac581584ede9672763ed
Irish Micole Cando <irishmicolecando@gmail.com>
2023-03-18 22:00:20 +0800

    Declare needed variables

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 'use strict'
+
+const ratingStateDiv = document.getElementById("rating-state-container");
+const ratingButtons = document.querySelectorAll(".rating-btns");
+const submitButton = document.getElementById("submit-btn");
+const thankYouStateDiv = document.getElementById("thank-you-state-container");
+const rating = document.getElementById("rating");
