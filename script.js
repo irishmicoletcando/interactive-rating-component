1706d91b9b0834b7221e703cd56e11618a8da481
Irish Micole Cando <irishmicolecando@gmail.com>
2023-03-18 22:01:09 +0800

    Add and remove a class when user clicks a rating button

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ const ratingButtons = document.querySelectorAll(".rating-btns");
 const submitButton = document.getElementById("submit-btn");
 const thankYouStateDiv = document.getElementById("thank-you-state-container");
 const rating = document.getElementById("rating");
+
+let selectedRating = 0;
+
+ratingButtons.forEach(ratingButton => {
+  ratingButton.addEventListener("click", function() {
+     // Remove "rating-btn-selected" class from all rating buttons
+    ratingButtons.forEach(ratingButton => {
+      ratingButton.classList.remove("rating-btn-selected");
+    })
+    // Add "rating-btn-selected" class to the clicked rating button
+    ratingButton.classList.add("rating-btn-selected");
+
+    selectedRating = Number(ratingButton.textContent);
+  })
+})
